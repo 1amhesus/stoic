49c5e69239f85eebcc820482c6ec3bb52f1d860a
1amhesus <iamhesus443@gmail.com>
2025-06-20 16:58:54 +0900

    Add project files to version control

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+#!/usr/bin/env rust
+
+use rand::seq::SliceRandom;
+use rand::thread_rng;
+
+fn main() {
+    let stoic_quotes = vec![
+        "The mind is everything; what you think, you become. - Buddha",
+        "Simplicity is the ultimate sophistication. - Leonardo da Vinci",
+        "Be present above all else. - Naval Ravikant",
+        "The obstacle is the way. - Marcus Aurelius",
+        "Know yourself. - Socrates",
+        "It's not what happens to you, but how you react to it that matters. - Epictetus",
+        "The only true wisdom is in knowing you know nothing. - Socrates",
+    ];
+
+    let mut rng = thread_rng();
+    let quote = stoic_quotes.choose(&mut rng).unwrap();
+
+    println!("{}", quote);
+}
+
